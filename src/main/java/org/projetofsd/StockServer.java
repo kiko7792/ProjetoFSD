e598d27f83e78b3bcade1a529bf20a76f78b5abb
kiko7792 <josebarreirinho92@gmail.com>
2023-10-06 15:01:32 +0100

    Criar Classes

--- src/main/java/org/projetofsd/StockServer.java
+++ src/main/java/org/projetofsd/StockServer.java
@@ -1,42 +1,40 @@
 package org.projetofsd;
+import org.projetofsd.Stock;
+
 import java.net.*;
 import java.io.*;
-    public class StockServer {
-        static int DEFAULT_PORT=2000;
 
-        public static void main(String[] args) throws IOException {
-            int port=DEFAULT_PORT;
-            Stock presences = new Stock();
+public class StockServer {
+    static int DEFAULT_PORT=2001;
 
-            ServerSocket servidor = null;
+    public static void main(String[] args) throws IOException {
+        int port=DEFAULT_PORT;
+        Stock stock = new Stock();
+
+        ServerSocket servidor = null;
 
 // Create a server socket, bound to the specified port: API java.net.ServerSocket
-            ServerSocket welcomeSocket = new ServerSocket(port);
+        ServerSocket welcomeSocket = new ServerSocket(port);
 
-            System.out.println("Servidor a' espera de ligacoes no porto " + port);
+        System.out.println("Servidor a' espera de ligacoes no porto " + port);
 
-            while(true) {
-                try {
+        while(true) {
+            try {
 
 // Listen for a connection to be made to the socket and accepts it: API java.net.ServerSocket
 
-                    Socket socketPedido = welcomeSocket.accept();
+                Socket socketPedido = welcomeSocket.accept();
 
 // Start a GetPresencesRequestHandler thread
+                GetPresencesRequestHandler GPRH = new GetPresencesRequestHandler(socketPedido, presences);
+                GPRH.start();
 
-                    BufferedReader in =
-                            new BufferedReader(new InputStreamReader(socketPedido.getInputStream()));
-                    PrintWriter out = new PrintWriter(socketPedido.getOutputStream(), true);
 
-                    String pedido = in.readLine();
-                    String resposta = pedido.toUpperCase();
-                    out.println(resposta);
-                    socketPedido.close();
-
-                } catch (IOException e) {
-                    System.out.println("Erro na execucao do servidor: "+e);
-                    System.exit(1);
-                }
+            } catch (IOException e) {
+                System.out.println("Erro na execucao do servidor: "+e);
+                System.exit(1);
             }
+
         }
+    }
 }
